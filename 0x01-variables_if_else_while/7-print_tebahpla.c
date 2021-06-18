6a3ba172cb63d1c5c0424e55601b021c613838ee
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-18 20:23:48 +0100

    Update 7-print_tebahpla.c
    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - prints out the alphabet in lowercase and then uppercase
+ * main - prints out the alphabet in reverse
  * Return: 0
  */
 int main(void)
 {
-    char z = 'z';
-    while(z>='a'){
-        putchar(z); 
-        z--;
-    }
-    return (0);
+	char z = 'z';
+
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+
+	putchar('\n');
+	return (0);
 }
