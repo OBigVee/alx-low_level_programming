c853d8b3ba5628215995af7eeeebcbf64d434598
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-18 18:37:56 +0100

    Create 3-print_alphabets.c
    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+ 
+#include <stdio.h>
+
+/**
+ * main - prints out the alphabet in lowercase and then uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	A = 'A';
+	while (A <= 'Z')
+	{
+
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
