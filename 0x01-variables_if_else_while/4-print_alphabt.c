aa4c60e520fcd15de95aa19ea829c79ea897a778
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-18 19:23:07 +0100

    Update 4-print_alphabt.c
    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - prints out the alphabet in lowercase and then uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+    if (a!='e' && a!='q'){
+        putchar(a);
+    }
+    a++;
+	}
+	putchar('\n');
+	return (0);
+}
