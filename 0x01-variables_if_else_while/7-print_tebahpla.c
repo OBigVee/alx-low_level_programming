9df4dd9f8312858b9f11e4d2c6f963a49e5bbc0b
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-18 20:16:30 +0100

    Create 7-print_tebahpla.c
    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints out the alphabet in lowercase and then uppercase
+ * Return: 0
+ */
+int main(void)
+{
+    char z = 'z';
+    while(z>='a'){
+        putchar(z); 
+        z--;
+        putchar('\n');
+    }
+    return (0);
+}
