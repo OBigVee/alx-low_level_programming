c49bb8b86c4622c5dbfc44e31ef6c07d4884777f
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-22 21:09:08 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 int idx;
 for (idx = 0 ; (idx < (int)strlen(alphabet) - 4) ; idx++)
 {
- _putchar(alphabet[idx]);
+_putchar(alphabet[idx]);
 }
 _putchar('\n');
 }
