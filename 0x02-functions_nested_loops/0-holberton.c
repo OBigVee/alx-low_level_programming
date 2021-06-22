f4810f63bf859ecd9abe5789400e4290e13e7915
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-22 16:57:03 +0100

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,19 @@
-#include"holberton.h"
+#include "holberton.h"
 
-/** 
+/**
  * main - Entry point
  * Return:0
  */
 
- int main(void)
- {
- char str[10] = "Holberton";
- int idx = 0;
- while (idx < 9)
- {
- _putchar(str[idx]);
- idx++;
- }
- _putchar('\n');
- return (0);
- }
+int main(void)
+{
+char str[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
