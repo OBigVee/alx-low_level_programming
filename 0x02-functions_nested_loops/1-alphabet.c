a7d95689add82b8656c68b417c0284c5d8b20f20
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-22 19:29:07 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include <string.h>
 #include"holberton.h"
 
-/*
- * main - Entry
- * return - 0
- * */
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
 int print_alphabet(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int idx = 0;
 	while (idx < (int) strlen(alphabet))
-			{
-			_putchar(alphabet[idx]);
-			idx++;
-
-			}
+	{
+		_putchar(alphabet[idx]);
+		idx++;
+	}
 	_putchar('\n');
 	return(0);
 }
