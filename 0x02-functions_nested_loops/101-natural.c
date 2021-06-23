5a86e4435356368cc7c546aa5f9aa01b1992f3a3
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-23 21:44:16 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,20 +6,15 @@
 */
 
 int main(void){
-int sum3;
-int sum5;
+int sum_multiple3_5;
 int i;
-for(i=0; i<1024; ++i)
+for(i = 0; i < 1024; ++i)
 {
-if(i%3 == 0){
-sum3 += i;
-}
-else if (i%5 == 0)
+if(i % 3 == 0 || i % 5 == 0)
 {
-sum5 += i;
+sum_multiple3_5 += i;
 }
 }
-printf("%d",sum3);
+printf("%d",sum_multiple3_5);
 return (0);
-
 }
