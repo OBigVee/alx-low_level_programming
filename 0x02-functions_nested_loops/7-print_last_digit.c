9c5f275b1ba19ab9256fae143e4312051ad48c79
Victor Adedeji <adedejivictor98@gmail.com>
2021-06-23 17:28:34 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,8 @@
 int print_last_digit(int n)
 {
   int lastDigit;
-  lastDigit = n%10 //
-  if(lastDigit<0)// -ve take the absolute
+  lastDigit = n%10  
+  if(lastDigit<0)
   { 
    lastDigit =  abs(lastDigit);
   }
