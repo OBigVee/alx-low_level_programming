a3f9bfbddb2460b0e0ce5189a86126dcdb8ddda6
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-08 21:39:47 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
 
-/** 
-* _strlen_recursion - returns the len of string
-* @s: string to check 
-* Return: string len
-*/
-
+/**
+ * _strlen_recursion - returns the len of a string
+ * @s: string to check
+ *Return: string len
+ */
 int _strlen_recursion(char *s)
 {
-if(*s == '\0')
-{
-return(0);
-}
-return(1+_strlen_recursion(s+1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
