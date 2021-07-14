2425575a59edd75ba95f4afdc65cb3ddb9472d49
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-14 13:17:23 +0100

    curly bracket fixed

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -6,18 +6,17 @@
  *     */
 int _atoi(char *s)
 {
-	        unsigned int num = 0, sign = 1, started = 0;
-
-		        while (*s)
-				        {
-						                if (started && !(*s >= '0' && *s <= '9'))
-									                        break;
-								                if (*s == '-')
-											                        sign *= -1;
-										                if ((*s >= '0' && *s <= '9'))
-													                {
-																                        started = 1;
-																			                        num =  num * 10 + (int)*s - 48;
-																						                }
-												                s++;
-														        }
+unsigned int num = 0, sign = 1, started = 0;
+	while (*s)
+	{
+		if (started && !(*s >= '0' && *s <= '9'))
+			break;
+		if (*s == '-')
+			if ((*s >= '0' && *s <= '9'))
+			{
+				started = 1;
+				num =  num * 10 + (int)*s - 48;
+			}
+		s++;
+	}
+}
