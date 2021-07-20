f5c6e6712b76d747c331de1ed7a08c717c01a7e4
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-20 15:49:28 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,19 +2,20 @@ nclude "holberton.h"
 #include <stdio.h>                                                                                               
                                                                                                                  
 /**                                                                                                              
- *  *_strcat: fun which prints the concat of two strings                                                            
- *   * @dest: pointer  destination                                                                                   
- *    * @src: ointer source                                                                                           
- *     */                                                                                                              
+ *_strcat: fun which prints the concat of two strings                                                            
+ *@dest: pointer  destination                                                                                   
+ *@src: ointer source                                                                                           
+ */
+	
 char _strrcat(char *dest, char *src)                                                                             
-{                                                                                                                
-	int a = -1;                                                                                                      
-	int i;                                                                                                           
-	for(i=0; dest[i] != '\0'; i++)                                                                                   
-		        do{                                                                                                      
-				                a++ ;                                                                                      						                dest[i] = src[a] ;                                                                               
-								                i++ ;                                                                                            
-										        }                                                                                                        
-	        while(src[a] != '\0');                                                                                   
-	                                                                                                                 return(dest);
+{                                                                                                              
+int a = -1;                                                                                                      
+int i;                                                                                                           
+for(i=0; dest[i] != '\0'; i++)                                                                                   
+do{                                                                                                      
+a++ ;
+dest[i] = src[a] ;                                                                               
+i++ ;
+while(src[a] != '\0');                                                                                   
+return(dest);
 }  
