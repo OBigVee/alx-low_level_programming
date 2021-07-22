e68f6524961a29e7954f99e1e9fe06dcff112308
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-22 22:35:26 +0100

    betty compliant geez

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * create_array - function creates an array with init values 
- * and retrun 0 if size == 0
- * @size: size of array
- * @c: char to init
- * Return: poonter to the array init or NULL
+ *create_array - function creates an array with init values 
+ *and retrun 0 if size == 0
+ *@size: size of array
+ *@c: char to init
+ *Return: poonter to the array init or NULL
  */
 
-char *create_array(unsigned int size, char c){
-    char *array = malloc(sizeof(size)*size);
-    if (size == 0)
-    {
-        return (0);
-    }
-    while(size--){
-        array[size] = c;
-    }
-    return (array);
+char *create_array(unsigned int size, char c)
+{
+char *array = malloc(sizeof(size)*size);
+if (size == 0)
+{
+return (0);
+}
+while(size--){
+array[size] = c;
+}
+return (array);
 }
