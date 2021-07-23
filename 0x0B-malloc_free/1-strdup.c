b61df89d955ed4275d30bb19da8566527e73bc87
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-23 13:43:05 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,12 @@ char *_strdup(char *str)
 int i = 0;
 int size = 0;
 char *m;
-  
 if (str == NULL)
 {
-return(NULL);
+return (NULL);
 }
 for (; str[size] != '\0'; size++);
-m = malloc(size * sizeof(str) + '\0');
+m = malloc (size * sizeof(str) + '\0');
 if (m == 0)
 return (NULL);
 else
