6b1a78f0322c80af9d85c59f3ef11a3960498ad1
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-24 12:13:24 +0100

    If you even dream of beating me you'd better wake up and apologize
    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * **alloc_grid - function creates an array with init values 
+ * and retrun 0 if size == 0
+ * @width: size of array
+ * @height: char to init
+ * Return: pointer to the array init or NULL
+ */
+
+int **alloc_grid(int width, int height){
+    int **m;    
+    width = 0;
+    height = 0;
+    int row = 0;
+    int col = 0;
+    if (width == NULL || height == NULL || width <= 0 || height <= 0){
+        return  (NULL);
+        }
+
+        /* create a memory */
+        m = malloc(sizeof(*m) * height);
+        if (!m){
+            return (NULL);
+        }
+        else{
+            /*ROW*/
+            for(; row < width; ++row){
+                /* create or allocate memory for type int * the number of width*/
+                    m[row] = malloc(sizeof(** m)*width);
+                    if(m[row] == 0){
+                        /*free mem*/
+                        while(row--){
+                            free(m[row]);
+                            }
+                        free(m);
+                        return (NULL);
+                        }
+                    for (; col < height; ++col){
+                        m[row][col] = 0;
+                        }
+                    }
+            }
+    return(0);
+}
