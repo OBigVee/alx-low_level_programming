e7c9d29801ab48b59f5f2223a4f26522f711b93b
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-29 14:34:49 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,49 @@
 #include "holberton.h"
+
 /**
- * jack_bauer - prints a 24 hour timer starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return:void
  */
+
 void jack_bauer(void)
 {
-	int hour, minutes;
 
-	for (hour = 0; hour < 24; hour++)
+int h1;
+int h2;
+int m1;
+int m2;
+int a = 9;
+
+h2 = 0;
+while (h2 <= 2)
+{
+	if (h2 == 2)
 	{
-		for (minutes = 0; minutes < 60; minutes++)
-		{
-			_putchar(hour / 10 + 48);
-			_putchar(hour % 10 + 48);
-			_putchar(':');
-			_putchar(minutes / 10 + 48);
-			_putchar(minutes % 10 + 48);
-			_putchar('\n');
-		}
+		/*Restrain to 23h, not 29*/
+		a = 3;
 	}
+	h1 = 0;
+	while (h1 <= a)
+	{
+		m2 = 0;
+		while (m2 <= 5)
+		{
+			m1 = 0;
+			while (m1 <= 9)
+			{
+				_putchar('0' + h2);
+				_putchar('0' + h1);
+				_putchar(':');
+				_putchar('0' + m2);
+				_putchar('0' + m1);
+				_putchar('\n');
+				m1++;
+			}
+			m2++;
+		}
+		h1++;
+	}
+	h2++;
+}
+
 }
