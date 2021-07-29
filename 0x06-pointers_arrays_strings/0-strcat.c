5db17947cfb816bb0d3a7f065414e2a05a15f92f
Victor Adedeji <adedejivictor98@gmail.com>
2021-07-29 14:41:19 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,22 @@
-#include "holberton.h"                                                                                           
-#include <stdio.h>                                                                                               
-                                                                                                                 
-/**                                                                                                              
- *_strcat: fun which prints the concat of two strings                                                            
- *@dest: pointer  destination                                                                                   
- *@src: ointer source                                                                                           
- */
-	
-char _strcat(char *dest, char *src)                                                                             
-{                                                                                                              
-int a = -1;                                                                                                      
-int i;                                                                                                           
-for(i=0; dest[i] != '\0'; i++)                                                                                   
-do{                                                                                                      
-a++ ;
-dest[i] = src[a] ;                                                                               
-i++ ;
-while(src[a] != '\0');                                                                                   
-return(dest);
-}  
+#include "holberton.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
+}
