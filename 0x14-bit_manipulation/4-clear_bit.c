6f91d6d28a3f9fcdb5c58e393b425facb55a2a20
Victor Adedeji <adedejivictor98@gmail.com>
2021-08-08 12:36:01 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,6 +8,7 @@
  * Return: If an error occurs - -1.
  *         Otherwise - 1.
  */
+#define BYTE_LENGHT 8
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
