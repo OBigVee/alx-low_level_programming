5ed3493723262058f45b8470ab26c3fdbd9628da
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-24 18:28:44 +0100

    puts

--- 0x18-dynamic_libraries/_puts.c
+++ 0x18-dynamic_libraries/_puts.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
-void _puts(char *s)
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to be printed
+ */
+void _puts(char *str)
 {
-
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
