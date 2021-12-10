b7e15445002694172c35bf33d571e8bf60656473
Victor Adedeji <adedejivictor98@gmail.com>
2021-12-11 00:34:23 +0100

    betty comply

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,16 +3,15 @@
 /**
  * print_dlistint - print all the elements of a dlistint_t list
  * @h: pointer to the start of the linked list
- * 
  * Return: number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nbr_nodes;
-	for(nbr_nodes=0; h != NULL; ++nbr_nodes)
+	for( nbr_nodes=0; h != NULL; ++nbr_nodes)
 	{
-		printf("%d\n", h->n);
+		printf( "%d\n", h->n);
 		h = h->next;
 	}
 	return (nbr_nodes);
