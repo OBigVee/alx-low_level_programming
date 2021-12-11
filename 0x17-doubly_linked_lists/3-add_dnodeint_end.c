980e4bd4ecbadf067179c0278887643415150aa4
Victor Adedeji <adedejivictor98@gmail.com>
2021-12-11 01:15:36 +0100

    betty comply geez

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,13 +11,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
     dlistint_t *new, *tmp;
 
-    if (head == NULL){
+    if (head == NULL)
         return (NULL);
-    }
     new = malloc(sizeof(dlistint_t));
-    if (new == NULL){
+    if (new == NULL)
         return (NULL);
-    }
     new->n = n;
     new->next = NULL;
     if (*head == NULL){
@@ -27,9 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
     }
     tmp = *head;
     while (tmp->next != NULL)
-    {
         tmp = tmp->next;
-    }
     tmp->next = new;
     new->prev = tmp;
     return (new);
