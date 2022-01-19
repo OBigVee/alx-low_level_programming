c5e316f58ab281788640dca4906285ea566a07c8
Victor Adedeji <adedejivictor98@gmail.com>
2022-01-20 00:37:46 +0100

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 size_t i = 0;
 if (array == NULL || size == 0)
 return (-1);
-while (i < size)
+for (i; i < size; ++i)
 {
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
