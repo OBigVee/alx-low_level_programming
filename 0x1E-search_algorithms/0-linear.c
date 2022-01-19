ea4657f538333af34b16d805eda7cee8c7b6103f
Victor Adedeji <adedejivictor98@gmail.com>
2022-01-19 22:50:13 +0100

    white space

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * linear_search - search for a value in an array of intergers
  * @array: pointer to the first element of the array to search in
@@ -13,14 +12,14 @@
  *  Everytime a value is compare in the array to the value you are searching the
  *  the function prints the value
  */
-
 int linear_search(int *array, size_t size, int value)
 {
     size_t i = 0;
     if (array == NULL || size == 0){
         return -1;
     }
-    while(i < size){
+    while(i < size)
+    {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if(array[i] == value)
             return i;
