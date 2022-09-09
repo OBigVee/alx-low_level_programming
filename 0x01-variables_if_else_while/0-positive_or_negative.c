a187d155af847457f212b6a3f3aea65f511649ea
Victor Adedeji <adedejivictor98@gmail.com>
2022-09-09 13:49:35 +0100

    task 0 completed :heavy_check_mark:
       - adde 0-0-positive_or_negative.c file                                
        - program checks if the n is +ve or -Ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Description: prints "is it positive or negative"
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
