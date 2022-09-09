072d3a7a1105bf254e3cb6e6ab411a787e82c567
obigvee <adedejivictor98@gmail.com>
2022-09-09 08:06:23 -0700

    task 6 completed :heavy_check_mark:

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry code
+ * Description: prints "single digit of base 10 from 0"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int z = 10';
+
+	while (a <= z)
+	{
+		putchar(49 + a);
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
