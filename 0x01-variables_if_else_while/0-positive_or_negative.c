9661f78851e1a192038c21fc8a47fd39d7ffdfc4
Victor Adedeji <adedejivictor98@gmail.com>
2022-09-09 13:51:06 +0100

    task 0 completed :heavy_check_mark:
    - adde 0-0-positive_or_negative.c file                             
            - program checks if the n is +ve or -Ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("is positive\n", n);
 	} else if (n < 0)
 	{
-		printf("is negative\n");
+		printf("is negative\n", n);
 	} else
 	{
-		printf("is zero\n");
+		printf("is zero\n", n);
 	}
 	return (0);
 }
