01f94cc07a46752a076a093f18a42556f4367be2
Victor Adedeji <adedejivictor98@gmail.com>
2022-09-11 21:15:47 +0100

    task 11 completed :heavy_check_mark:

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 /**
  *  main - Entry code
- *  Description: prints all possible combination of 2 digits
+ *  Description: prints all possible different combinations of three digits
  *  INTSTUCTIONS;
- *  the two digits must be different,
- *  01 and 10 are considered the same combination of the two digits 0 and 1
- *  Print only the smallest combination of two digits
- *  Numbers should be printed in ascending order, with two digits
- *  You can only use the putchar function --
- *	(every other function (printf, puts, etc…) is forbidden)
- *  You can only use putchar five times maximum in your code
- *  You are not allowed to use any variable of type char
- *  Return: 0
+ *  Numbers must be separated by ,, followed by a space
+ * The three digits must be different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination of the--
+ *        three digits 0, 1 and 2
+ * Print only the smallest combination of three digits
+ * Numbers should be printed in ascending order, with three digitsYou can--
+ *        only use the putchar function (every other function--
+ *        (printf, puts, etc…) is forbidden)
+ * You can only use putchar six times maximum in your code
+ * You are not allowed to use any variable of type char
  */
 int main(void)
 {
