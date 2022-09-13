cb40e24098547f0d0f81734868f264da7bfbfac2
obigvee <adedejivictor98@gmail.com>
2022-09-13 03:27:45 -0700

    task 1 completed :heavy_check_mark:
    - added file 0-putchar.c
    - program use main.h prototype to print array of char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - entry code
+ * Description: print _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	size_t i;
+	char str[] = "_putchar";
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+		_putchar('\n');
+	}
+	return (0);
+}
