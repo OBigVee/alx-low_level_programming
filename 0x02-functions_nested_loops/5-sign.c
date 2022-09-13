594d24a5674064b69ba92a5195d4ef899d989c72
obigvee <adedejivictor98@gmail.com>
2022-09-13 12:04:18 -0700

    task 5 completed :heavy_check_mark:
    - added 5-sign.c file
    function prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - function prints the sign of a number
+ * @n: number to check
+ * Return: 1,0,-1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (-1);
+}
