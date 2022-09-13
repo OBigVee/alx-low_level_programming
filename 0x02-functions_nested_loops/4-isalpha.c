254018edfb320c0f3e0613a219fdbf92c0964f03
obigvee <adedejivictor98@gmail.com>
2022-09-13 11:52:35 -0700

    task 4 completed :heavy_check_mark:
    - added 4-isalpha.c file
    - function checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 /**
  * _isalpha - function checks for alphabetic character
- * @c : character to check 
+ *@c : character to check
  * Return: 1 if c is a letter, lowercase or uppercase else 0
  */
 int _isalpha(int c)
