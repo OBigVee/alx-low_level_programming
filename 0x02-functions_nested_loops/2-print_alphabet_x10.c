076a82608c2934e80ce04db9ceb0efc3b0071ae7
obigvee <adedejivictor98@gmail.com>
2022-09-13 11:26:51 -0700

    task 2 completed :heavy_checj_mark:
    - added 2-print_alphabet_x10.c file
    function prints 10 times the alphabet in lowercase followed by new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - code entry
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char a;
+	char z = 'z';
+
+	i = 0;
+	while (i < 11)
+	{
+		for (a = 'a'; a <= z; a++)
+		{
+			_putchar(a);
+		}
+		i++;
+		_putchar('\n');
+	}
+}
