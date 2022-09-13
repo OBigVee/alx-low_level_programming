9c374c172b4c5e367bc6edc02e29d0f56ebe1c06
obigvee <adedejivictor98@gmail.com>
2022-09-13 15:29:38 -0700

    task 10 completed :heavy_check_mark:

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - add two int
+ * @a: fist val
+ * @b: second value
+ * Return: int
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
