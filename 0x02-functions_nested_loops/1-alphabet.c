641fe4c9653bbe79a0e89703d6818ea48063f60b
obigvee <adedejivictor98@gmail.com>
2022-09-13 11:16:06 -0700

    task 1 completed :heavy_check_mark:
    - added 1-alphabet.c file
    - program prints alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - entry code
+ * print_alphabet - entry code
  * Description: print print_alphabet- alphabet in lowercase.
  * Return: 0
  */
@@ -11,7 +11,7 @@ void print_alphabet(void)
 	char a = 'a';
 	char z = 'z';
 
-	for (a = 'a'; a < z; a++)
+	for (a = 'a'; a <= z; a++)
 	{
 		_putchar(a);
 	}
