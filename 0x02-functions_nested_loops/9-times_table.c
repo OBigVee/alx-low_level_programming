e85e5901e05df031f2d2f76c82459b9951bdc92b
obigvee <adedejivictor98@gmail.com>
2022-09-13 15:23:02 -0700

    task 9 completed :heavy_check_mark:

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,26 +6,30 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, result;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			int result = i * j;
-
-			if (result < 10)
+			if (j == 0)
 			{
-				_putchar(result + 48);
-				_putchar(',');
-				_putchar(' ');
+				_putchar(48);
+				continue;
 			}
-			else
+			result = i * j;
+			_putchar(',');
+			_putchar(' ');
+
+			if (result >= 10)
 			{
 				_putchar(result / 10 + 48);
 				_putchar(result % 10 + 48);
-				_putchar(',');
+			}
+			else
+			{
 				_putchar(' ');
+				_putchar(result + 48);
 			}
 		}
 		_putchar('\n');
