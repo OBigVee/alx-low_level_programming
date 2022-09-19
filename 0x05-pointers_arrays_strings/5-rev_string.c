5abd96676a86724e0977b1ee18b7254d1dfd10ff
OBigVee <adedejivictor98@gmail.com>
2022-09-19 17:05:31 +0100

    task 5 completed :heavy_check_mark:
    - added 5-rev_string.c
    function  reverses a string
    - added 5-main test case file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * rev_string - function reverses a string
+  * @s: string printed in reversed
+  * Return: 0
+  */
+
+void rev_string(char *s)
+{
+	print_rev(s);
+}
