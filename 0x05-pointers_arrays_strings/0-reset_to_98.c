1de3698f07192307d88150dda7df3b793c625ce2
OBigVee <adedejivictor98@gmail.com>
2022-09-19 15:51:30 +0100

    task 0 completed :heavy_check_mark:
     -added 0-reset_to_98.c file
    - program update the value of variable by reference

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function takes a pointer to an int as param
+ * and updates the value it points to 98
+ * @n: - pointer to int param
+ * Return - 0
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
