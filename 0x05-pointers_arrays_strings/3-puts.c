de8f99e52996c6256b2c96a9c10b82cde144dcb9
OBigVee <adedejivictor98@gmail.com>
2022-09-19 16:29:43 +0100

    task 3 completed :heavy_check_mark:
    - added 3-puts.c file
    - function prints a string
    - added 3-main.c test case file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts - function prints a string followed by a new line to stdout
+  * @str : string to be printed
+  * Return: 0
+  */
+
+void _puts(char *str)
+{
+	puts(str);
+}
