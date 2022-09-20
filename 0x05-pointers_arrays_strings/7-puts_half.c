6f2664966c99b3f29d7ecc816a0ee9053dc97087
OBigVee <adedejivictor98@gmail.com>
2022-09-20 10:15:03 +0100

    task 7 completed :heavy_check_mark:
    - added 7-puts_half.c file
    - function prints half of a string
    - added 7-main.c test case file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * puts_half - function prints half of a string,
+  * followed by a new line.
+  * @str: string to be processed
+  * Return: 0
+  */
+void puts_half(char *str)
+{
+	int i, size;
+
+	size = strlen(str);
+	i = size / 2;
+	while (i <= size)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
