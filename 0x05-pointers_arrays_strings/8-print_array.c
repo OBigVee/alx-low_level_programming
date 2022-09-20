4f6ffbd84275e4cc6b1ae22666bb35bbeccce129
OBigVee <adedejivictor98@gmail.com>
2022-09-20 12:09:02 +0100

    task 8 completed :heavy_check_mark:
    - added 8-print_array.c file
    - function prints 'n' elements of an array of int followed by new line.
    - added 8-main.c test case file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_array - function prints n elements of
+  * an array of int, followed by a new line.
+  * @a: array
+  * @n: is the number of elements of the array
+  * to be printed
+  * nums must b seprated by comma, followed by
+  * a space
+  * The numbers should be displayed in the same
+  * order as they are stored in the array
+  * you are allowed to use printf
+  * Return:0
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (n - 1 == i)
+		{
+			printf("%d", a[i]);
+		}
+		if (i < n - 1)
+		{
+			printf("%d,", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
+
