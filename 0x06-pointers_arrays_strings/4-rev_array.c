284bbca7dd2a9baac8a05e2e75f903b4a8f8b827
OBigVee <adedejivictor98@gmail.com>
2022-09-22 18:29:29 +0100

    task 4 completed :heavy_check_marl:
    - added 4-rev_array.c file
    - function reverses content of an array of integers
    - added 4-main.c test case file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 {
 	int i, temp;
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i <= n -1; i++)
 	{
 		temp = a[i];
-		*(a + i) = *(a + n);
-		*(a + n) = temp;
+		*(a + i) = *(a + n - 1);
+		*(a + n - 1) = temp;
 		n--;
 	}
 }
