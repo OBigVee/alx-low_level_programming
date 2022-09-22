da54351e258ab8f72a8d3b80b99e577a21f85c4c
obigvee <adedejivictor98@gmail.com>
2022-09-22 03:57:52 -0700

    task 2 completed :heavy_check_mark:
    - added 1-strncat.c file
    - function concat two strings using n bytes from src pointer
    - added 1-main.c test case file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strncat - function concat two strings
+ * The _strncat function is similar to the _strcat
+ * _ function, except that
+ * ^ it will use at most n bytes from src; and
+ * ^src does not need to be null-terminated if it
+ * _ contains n or more bytes
+ * @dest: pointer to concat string with
+ * @src: pointer to copy string from
+ * @n: at most bytes from src
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+	}
+	return dest;
+}
