ba5431f65f4d1e74379b3cacb6bd3faa0c1f2c32
OBigVee <adedejivictor98@gmail.com>
2022-09-23 17:38:59 +0100

    task 6 completed :heavy_check_mark:

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#define SIZE 14
+
+/**
+  * *cap_string - function capitalize all words
+  * __ of a string
+  * seprators of words: space, tab, newline
+  * __ , ; . ! ? " ( ) { }
+  * @n: string
+  * Return: char
+  */
+
+
+char *cap_string(char *n)
+{
+	int j;
+	size_t i;
+
+	char s_char[SIZE] = {',', ';', '!', '?', '"', '(', ')', '{', '}'
+		, ' ', '\n', '\t', '.'};
+
+	for (i = 0; i < strlen(n) && n[i] != '\0'; i++)
+	{
+		for (j = 0; j < SIZE; j++)
+		{
+			if (n[i] == s_char[j])
+			{
+				*(n + i + 1) -= 32;
+			}
+		}
+	}
+	return (n);
+}
