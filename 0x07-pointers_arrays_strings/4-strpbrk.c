de2d26ad4864a50ff28ba9d94d02b6b159410304
OBigVee <adedejivictor98@gmail.com>
2022-09-27 17:05:29 +0100

    task 4 completed :heavy_check_mark:
    - added 4-strpbrk.c file
    - function locates the first occurence in the string 's'of any of the bytes in the string 'accept'
    -function returns a pointer to the byte in 's' that matches one of the bytes in 'accept', or NULL if no such byte is found.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * *_strpbrk - function searches a string for any of a
+  * set of bytes.
+  * @s: pointer to string
+  * @accept: pointer to string to be searched.
+  * The function locates the first occurences in the
+  * string 's' of any of the bytes in the string 'accept'
+  * Return: a pointer to the byte in 's' that matches one of the bytes
+  * in 'accept', or NULL if no such byte is found.
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (s + i);
+}
