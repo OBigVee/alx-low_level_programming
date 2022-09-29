b3427f7bfe77601aac64b72a9bbce00495143d61
OBigVee <adedejivictor98@gmail.com>
2022-09-29 10:16:14 +0100

    task 5 completed :heavy_check_maark:
    - added 5-sqrt_recursion.c file
    - function computes the natural square root of a number
    - added 5-main.c test case file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,18 +3,27 @@
 #include <math.h>
 
 /**
+ * check_square - function checks if the multiplication of 2 int eqauls to 'n'.
+ * @n: value to check for
+ * @val: value to be multiplied together.
+ * Return: int
+ */
+
+int check_square(int n, int val); /* prototype function*/
+
+
+/**
  * _sqrt_recursion - function returns the natural square root of a number.
  * @n: int whose sqrt will be computed.
  * if 'n' does not have a natural square root, the function returns -1.
  * Return: int
  */
 
-int check_square(int n, int val); /* prototype function*/
-
 int _sqrt_recursion(int n)
 {
 	return (check_square(n, 1));
 }
+
 
 /**
  * check_square - function checks if the multiplication of 2 int eqauls to 'n'.
