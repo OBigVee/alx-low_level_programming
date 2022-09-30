e889a06c5466ef559b858cf609a10ac6ac9cec17
OBigVee <adedejivictor98@gmail.com>
2022-09-30 23:23:11 +0100

    task 0 completed :heavy_check_mark:
    - added 0-whatsmyname.c file
    - program prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main - code entry 
-  * 
-  * Return - 0 or 1
-  */
+ * main - code entry
+ * program prints the new name, without having to compile it again. 
+ * Return: 0 or 1
+ */
 int main(int argc, char *argv[])
 {
-	/*(void)argc;*/
-	while (argc--)
-		printf("%s\n", *(argv++));
+	(void)argc;
+	printf("%s\n", *(argv++));
 	return (0);
 }
