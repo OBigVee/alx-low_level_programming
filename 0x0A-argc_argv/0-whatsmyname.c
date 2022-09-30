23dc83c60658186a0f2289b6d4ccd19d669d4196
OBigVee <adedejivictor98@gmail.com>
2022-09-30 23:31:04 +0100

    task 0 completed :heavy_check_mark:
    - added 0-whatsmyname.c file
    - program prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,9 @@
 
 /**
  * main - code entry
- * program prints the new name, without having to compile it again. 
+ * program prints the new name,without having to compile it again.
+ * @argc: counts the numbers of flags
+ * @argv: pointer to array of flags
  * Return: 0 or 1
  */
 int main(int argc, char *argv[])
