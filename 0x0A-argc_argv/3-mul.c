465845d3c80ab2d8c2b7da1ab4e95d31d2048325
OBigVee <adedejivictor98@gmail.com>
2022-10-01 02:58:03 +0100

    task 3 completed :heavy_check_mark:
    - added 3-mul.c file
    - program performs the multiplication of 2 integer values and print the result.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - code entry
+ * program multiplies 2 numbers
+ * program prints Error if it does not receive 2 arguments.
+ * @argc: counts the numbers of flags
+ * @argv: pointer to array of flags.
+ * Return: Error or 1
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, mul;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		mul = a * b;
+		printf("%d\n", mul);
+	} else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
+
