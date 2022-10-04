8c050c4a2ba97a5dc0e192520e6b8c4198fb35ea
OBigVee <adedejivictor98@gmail.com>
2022-10-04 14:59:28 +0100

    task 1 compeleted :heavy_check_mark:
    - added 1-strdup.c file
    - function returns a pointer to a newly allocated space in memory, which contains a copy of the string as a parameter.
    returns NULL if insufficient memory was available
    - added 1-main.c test case file.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - function returns a pointer to a new string
+ * which is a duplicate of the string 'str'.
+ * Memory for d new string is obtained with malloc,
+ * and can be freed with 'free'.
+ * @str: string that is to be copied into another memory.
+ * Return: NULL if str = NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *_str;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	_str = malloc(strlen(str));
+	for (i = 0; i < strlen(str); i++)
+	{
+		_str[i] = str[i];
+	}
+	_str[i] = '\0';
+	return (_str);
+}
