fcd5aaca38d8b6d189087f953885c8b68a5e8b60
OBigVee <adedejivictor98@gmail.com>
2022-10-05 20:50:15 +0100

    task 4 completed :heavy_check_mark:
    - added 4-free_grid.c file
    - function frees a 2-dim grid created with alloc_grid function
    - added 4-main.c test case file.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function frees a 2-dim grid created by alloc_grid function.
+ * @grid: pointer to pointer that points to the address of alloc_grid function.
+ * @height: height of the array
+ * Return: 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i, **new_matrix;
+
+	/*new_matrix = malloc(height * sizeof(*grid));*/
+	new_matrix = grid;
+
+	for (i = 0; i < height; i++)
+	{
+		free(new_matrix[i]);
+	}
+}
