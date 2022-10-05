916c8f8e76bb42cb55baee52b10db25b987c4e2c
OBigVee <adedejivictor98@gmail.com>
2022-10-05 15:49:00 +0100

    task 3 completed :heavy_check_mark:
    - added 3-alloc_grid.c file
    - function returns a pointer to a 2 dim array of integers.
    - added 3-main.c test case file.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - function returns a pointer to a 2 dim array of intergers
+ * each element of the grid should be initialized to 0
+ * @width: row of the matrix
+ * @height: column of the matrix
+ * Return: NULL on failure or pointer to a the 2 dim array.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **matrix;
+
+	matrix = malloc((height * width) * sizeof(int) + 1);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	if (matrix == 0)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		matrix[i] = malloc(width * sizeof(**matrix));
+		if (matrix[i] == 0)
+		{
+			while (i--)
+				free(matrix[i]);
+			free(matrix);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] =  0;
+		}
+	}
+	return (matrix);
+}
