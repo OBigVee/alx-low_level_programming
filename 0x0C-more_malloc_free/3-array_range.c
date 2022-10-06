9f186e96909f7f3b8f6473bea0646152156a5e42
OBigVee <adedejivictor98@gmail.com>
2022-10-06 19:24:42 +0100

    task 3 completed :heavy_check_mark:
    - added 3-array_range.c file
    - function creates an array of integer within the specified range of values.
    - added 3-main.c test case file

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *array_range - function creates an array of integers
+ * the created array will contain all the values from
+ *  'min' to max in ascending order.
+ *  @min: the minimum element in the array to be created
+ *  @max: the max element in the range of values to be created for the array.
+ *  Return: pointer to the newly created array
+ *  NUll if min > max
+ *  Null if malloc fails.
+ */
+
+int *array_range(int min, int max)
+{
+	int i, *mem_block;
+
+	if (min > max)
+		return (NULL);
+	mem_block = malloc(max * sizeof(max) + 1);
+	if (mem_block == 0)
+		return (NULL);
+	for (i = min; i < max + 1; i++)
+	{
+		mem_block[i] = i;
+	}
+	mem_block[i] = '\0';
+
+	return (mem_block);
+}
