139f70f63fbeed478650c94bde7ebb4da0b487c4
OBigVee <adedejivictor98@gmail.com>
2022-10-10 22:09:23 +0100

    task 2 completed
    - added 2-print_dog.c file
    - function prints struct dog
    - added 2-main.c test case file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct name
+ *
+ * Return : 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+				d->name != NULL ? d->name : "nil",
+				d->age,
+				d->owner != NULL ? d->name : "nill"
+		      );
+	}
+}
