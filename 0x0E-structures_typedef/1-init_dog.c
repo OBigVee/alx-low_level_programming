2f54b4ae61da0876ff54c5935d6914560cdf1a94
OBigVee <adedejivictor98@gmail.com>
2022-10-10 17:39:31 +0100

    task 1 completed :heavy_check_mark:
    - added 1-init_dog.c file
    - function initialize a variable of type struct dog
    - adde 1-main.c test case file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - function initialize a variable of type struct dog
+ * @d: name of struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+
+}
