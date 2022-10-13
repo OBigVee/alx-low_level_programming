585d97ae4ce3caff9ba8fe12b1d887a0aa8c586d
OBigVee <adedejivictor98@gmail.com>
2022-10-13 20:53:02 +0100

    task 0 completed :heavy_check_mark:
    - added 0-sum_them_all.c file
    - function return the sum of all its parameters.
    - added 0-main.c test case file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,22 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	
-	unsigned int i, sum;
+	va_list list;
+	unsigned int i, sum = 0;
 
-	/* variable ap has a pointer as it type*/
-	va_list ap;
+	va_start(list, n);
+	if (n != 0)
+		for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+			;
+	va_end(list);
 
-	if (n == 0)
-		return (0);
-
-	/* n simply means argc i.e the number of arguments*/
-	va_start(ap, n);
-	sum = 0;
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(ap, unsigned int);
-		va_end(ap);
-	}
 	return (sum);
 }
