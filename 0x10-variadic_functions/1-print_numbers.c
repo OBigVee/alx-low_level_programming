c4379a7b761583354e4e51ccae3cbef7da507dc7
OBigVee <adedejivictor98@gmail.com>
2022-10-13 21:42:25 +0100

    task 1 completed :heavy_check_mark:
    - added 1-print_number.c file
    - function prints numbers followed by a new line
    - added 1-main.c test case file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function prints number by a new line.
+ * @seperator: is the string to be printed between numbers
+ * @n: is the number of integers passed to the function
+ * Return: 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+	if (n != 0)
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(list, int));
+			if (i != n-1 && separator != NULL)
+				printf("%s", separator);
+		}
+	va_end(list);
+	putchar('\n');
+}
