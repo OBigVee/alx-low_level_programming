7c1f504182d75333386bbcf96874491cd349752c
OBigVee <adedejivictor98@gmail.com>
2022-10-15 07:26:56 +0100

    header file updated :wrench:

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+void print_int(va_list list);
+void print_float(va_list list);
+void print_char(va_list list);
+void print_string(va_list list);
+
+
+typedef struct typesToPrintStruct
+{
+	char *type;
+	void (*printer)(va_list);
+} typesToPrintStruct;
+
 #endif /* VARIADIC_FUNCTIONS_H */
