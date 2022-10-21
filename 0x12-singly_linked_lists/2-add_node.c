a1a6569b4f6334ac3844b99d93dd794b403101d7
OBigVee <adedejivictor98@gmail.com>
2022-10-21 20:59:27 +0100

    task 2 completed :heavy_check_mark:
    - added 2-add_node.c file
    - function adds a new node at the begining of a 'list_t' list
    - function returns the address of the new element or NULL if it failed
    - added 2-main.c test case file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - function adds a new_node node at the begining of a list_t list.
+ * @head: pointer to head
+ * @str: data
+ * Return: address of the new_node element, or NULL if it failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int str_len;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+
+	}
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		return (NULL);
+	}
+	for (str_len = 0; str[str_len] != '\0'; str_len++)
+		;
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
