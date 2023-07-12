42812e75f52fea7c0126e9d500c348a60cdb8810
OBigVee <adedejivictor98@gmail.com>
2023-07-12 19:10:50 +0100

    header  file added :package: :heavy_check_mark:

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,11 @@
-#ifndef _SEARCH_ALGOS_H_
-#define _SEARCH_ALGOS_H_
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+
 #include <stdio.h>
 #include <math.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+#endif
