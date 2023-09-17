ece92e4103b17a91a55f42a4f9fa4315edeacb6f
obigvee <adedejivictor98@gmail.com>
2023-09-17 09:48:42 -0700

    :package:

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-oid close_fd(int fd);
+void close_fd(int fd);
 void copy_contents(int from_fd, int to_fd, char *src_file, char *dest_file);
 void close_fd(int fd);
 char is_elf_file(int fd, void **header);
